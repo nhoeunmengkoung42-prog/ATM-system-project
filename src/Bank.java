5b401a7a33587eb9d7eaeb358c75072cbd876912
samnangkong <samnangkong369@gmail.com>
2026-09-23 00:34:59 +0700

    update

--- src/Bank.java
+++ src/Bank.java
@@ -42,8 +42,6 @@ class Bank {
         }
     }
 
-    
-
     private Account buildAccountFromRow(ResultSet rs) throws SQLException {
         String number = rs.getString("account_number");
         String pin = rs.getString("pin");
